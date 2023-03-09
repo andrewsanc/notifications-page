fb10cdb3b3b1936b488d937eeefe88b94d727b5b
Andrew Sanchez <druu831@gmail.com>
2023-03-08 20:07:47 -0800

    hello world route

--- server/index.js
+++ server/index.js
@@ -3,4 +3,6 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => res.send("Hello World"));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
