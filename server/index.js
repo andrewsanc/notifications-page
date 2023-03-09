88410255083e27512189ede278b00dc4759fad14
Andrew Sanchez <druu831@gmail.com>
2023-03-08 20:01:59 -0800

    server refactor. update packagejson scripts

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-require("dotenv").config();
-
+const dotenv = require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
-const app = express();
-
-app.listen(port, console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
